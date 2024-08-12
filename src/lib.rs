343db1e7cab3525ff7ef442c78d7389715de39f0
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-12 19:39:43 +0200

    Fixed big mistake in documentation The wrong crate was mentioned in the crate root comment, must've forgot to change it after copying over from `argentum_game_voxel` crate...

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! # Argentum - Coordinate System
 //!
-//! `argentum_game_voxel` contains the coordinate system that is used by
+//! `argentum_game_coordinate_system` contains the coordinate system that is used by
 //! Argentum and some utilities related to it.
 //!
 //! For more information about Argentum, see the `argentum_game` crate.
