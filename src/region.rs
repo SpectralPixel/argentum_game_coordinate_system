7bc3d3fae03b5b13d7d3bb8bfe8848b0fabc950c
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-13 20:16:18 +0200

    Implemented a constructor for `Region`

--- src/region.rs
+++ src/region.rs
@@ -1,5 +1,27 @@
 use crate::CoordinateType;
 
+#[derive(PartialEq, Debug)]
 pub struct Region {
     size: CoordinateType,
+}
+
+impl Region {
+    pub fn new(size: CoordinateType) -> Self {
+        Self { size }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use quickcheck::quickcheck;
+
+    use super::*;
+
+    quickcheck! {
+        fn new_region(size: CoordinateType) -> bool {
+            let result = Region::new(size);
+            let expected = Region { size };
+            result == expected
+        }
+    }
 }
