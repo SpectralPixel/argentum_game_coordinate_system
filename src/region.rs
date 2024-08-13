14b5c94bb536791402eadbc8ea702fb2b045444b
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-13 20:30:47 +0200

    Added new fields to `Region` Currently the unit test for the constructor is relying on some band-aid fixes

--- src/region.rs
+++ src/region.rs
@@ -1,13 +1,19 @@
-use crate::CoordinateType;
+use crate::{Coordinate, CoordinateType};
 
 #[derive(PartialEq, Debug)]
 pub struct Region {
+    position: Coordinate,
     size: CoordinateType,
+    offset: Coordinate,
 }
 
 impl Region {
-    pub fn new(size: CoordinateType) -> Self {
-        Self { size }
+    pub fn new(position: Coordinate, size: CoordinateType) -> Self {
+        Self {
+            position,
+            size,
+            offset: Coordinate::new(0, 0, 0),
+        }
     }
 }
 
@@ -19,8 +25,9 @@ mod tests {
 
     quickcheck! {
         fn new_region(size: CoordinateType) -> bool {
-            let result = Region::new(size);
-            let expected = Region { size };
+            let coordinate = Coordinate::new(0,0,0);
+            let result = Region::new(coordinate.clone(), size);
+            let expected = Region { position: coordinate.clone(), size, offset: coordinate.clone() };
             result == expected
         }
     }
