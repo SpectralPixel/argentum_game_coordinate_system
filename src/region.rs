8b3008e2695121e09385ed8fc27223daf55f4242
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-14 21:35:12 +0200

    Renamed list in `Region` struct's doc test

--- src/region.rs
+++ src/region.rs
@@ -1,29 +1,29 @@
 use crate::{Coordinate, CoordinateType};
 
 /// Cube-shaped iterator of `Coordinate`s
-/// 
+///
 /// The iterator returns all `Coordinate`s from `position` to `position + size - 1`
-/// 
+///
 /// # Examples
-/// 
+///
 /// ```
 /// use argentum_game_coordinate_system::{Coordinate, Region};
-/// 
-/// let mut coord_vec: Vec<Coordinate> = Vec::new();
-/// coord_vec.push(Coordinate::new(7, 7, 7));
-/// coord_vec.push(Coordinate::new(8, 7, 7));
-/// coord_vec.push(Coordinate::new(7, 8, 7));
-/// coord_vec.push(Coordinate::new(8, 8, 7));
-/// coord_vec.push(Coordinate::new(7, 7, 8));
-/// coord_vec.push(Coordinate::new(8, 7, 8));
-/// coord_vec.push(Coordinate::new(7, 8, 8));
-/// coord_vec.push(Coordinate::new(8, 8, 8));
-/// 
+///
+/// let mut positions: Vec<Coordinate> = Vec::new();
+/// positions.push(Coordinate::new(7, 7, 7));
+/// positions.push(Coordinate::new(8, 7, 7));
+/// positions.push(Coordinate::new(7, 8, 7));
+/// positions.push(Coordinate::new(8, 8, 7));
+/// positions.push(Coordinate::new(7, 7, 8));
+/// positions.push(Coordinate::new(8, 7, 8));
+/// positions.push(Coordinate::new(7, 8, 8));
+/// positions.push(Coordinate::new(8, 8, 8));
+///
 /// let mut i = 0;
 /// let region = Region::new(Coordinate::splat(7), 2);
-/// for coord in region {
+/// for pos in region {
 ///     println!("{i}");
-///     assert_eq!(coord, coord_vec[i]);
+///     assert_eq!(pos, positions[i]);
 ///     i += 1;
 /// }
 /// ```
@@ -37,7 +37,7 @@ pub struct Region {
 
 impl Region {
     /// Creates a new Region
-    /// 
+    ///
     /// - `position` corresponds to the starting position of the iterator.
     /// - `size` detemines the range of the iterator. The range is exclusive.
     pub fn new(position: Coordinate, size: CoordinateType) -> Self {
@@ -56,7 +56,7 @@ impl Iterator for Region {
     fn next(&mut self) -> Option<Self::Item> {
         if self.first_iteration {
             self.first_iteration = false;
-            return Some(self.position.to_owned())
+            return Some(self.position.to_owned());
         }
 
         fn increment(n: &mut CoordinateType, size: CoordinateType) -> bool {
