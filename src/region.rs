26f74b247c9d1f415e0a838fc0bac271e630f8e2
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-14 19:28:13 +0200

    Implemented `Iterator` for Region

--- src/region.rs
+++ src/region.rs
@@ -17,6 +17,32 @@ impl Region {
     }
 }
 
+impl Iterator for Region {
+    type Item = Coordinate;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        fn increment(n: &mut CoordinateType, size: CoordinateType) -> bool {
+            *n = (*n + 1) % size;
+            *n == 0
+        }
+
+        let wrapped_x = increment(&mut self.offset.x, self.size);
+        let mut wrapped_y = false;
+        let mut wrapped_z = false;
+
+        if wrapped_x {
+            wrapped_y = increment(&mut self.offset.y, self.size);
+        }
+        if wrapped_y {
+            wrapped_z = increment(&mut self.offset.z, self.size);
+        }
+        match wrapped_z {
+            false => Some(self.position.to_owned() + self.offset.to_owned()),
+            true => None,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use quickcheck::quickcheck;
@@ -29,5 +55,14 @@ mod tests {
             let expected = Region { position, size, offset: Coordinate::new(0,0,0), };
             result == expected
         }
+    }
+
+    #[test]
+    fn iterate() {
+        let region = Region::new(Coordinate::new(0, 0, 0), 10);
+        region
+            .into_iter()
+            .filter(|pos| pos.x == 0)
+            .for_each(|pos| assert!(pos.x == 0));
     }
 }
