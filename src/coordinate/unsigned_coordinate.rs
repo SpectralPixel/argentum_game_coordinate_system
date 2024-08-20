e34c365fb140aedf74c44f3fa2d5b7b2b8704ae2
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-20 18:26:49 +0200

    Copied `base_coordinate` into both `unsigned_coordinate` and `signed_coordinate` and modified accordingly

--- src/coordinate/unsigned_coordinate.rs
+++ src/coordinate/unsigned_coordinate.rs
@@ -1,19 +1,13 @@
-use crate::generate_coordinate_type;
+use num::{Num, Unsigned};
 
 #[cfg(test)]
 mod tests;
 
-/// `Coordinate`'s field type.
-///
-/// i32: From −2,147,483,648 to 2,147,483,647.
-///
-/// I don't believe a larger size is necessary, as the RAM usage per instance
-/// would double. Heck, even this is already overkill.
-pub type CoordinateType = i32;
-
-generate_coordinate_type!(
-    Coordinate,
-    CoordinateType,
-    "3D Coordinate in absolute space.",
-    argentum_game_coordinate_system
-);
+struct UnsignedCoordinate<T>
+where
+    T: Num + Unsigned
+{
+    pub x: T,
+    pub y: T,
+    pub z: T,
+}
