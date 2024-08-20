e078d154d5f2d18586a06d17607ca034810b1072
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-20 18:41:15 +0200

    Fleshed out `CoordinateTrait` Added the `new` and `splat` methods

--- src/coordinate/coordinate_trait.rs
+++ src/coordinate/coordinate_trait.rs
@@ -1,3 +1,12 @@
 pub trait CoordinateTrait {
-    
+    type Type;
+    type FieldType;
+
+    fn new(
+        x: Self::FieldType,
+        y: Self::FieldType,
+        z: Self::FieldType,
+    ) -> Self::Type;
+
+    fn splat(n: Self::FieldType) -> Self::Type;
 }
