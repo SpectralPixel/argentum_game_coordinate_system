d61531246894fb8b267dbc5d8045034f46a9933e
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-21 13:16:50 +0200

    Implemented `CoordinateTrait` on `*signedCoordinate` via derive macro

--- src/coordinate/signed_coordinate.rs
+++ src/coordinate/signed_coordinate.rs
@@ -1,12 +1,15 @@
 use argentum_game_coordinate_system_macros::Coordinate;
 use num::{Num, Signed};
 
+use super::coordinate_trait::CoordinateTrait;
+
 #[cfg(test)]
 mod tests;
 
+#[derive(Coordinate)]
 pub struct SignedCoordinate<T>
 where
-    T: Num + Signed,
+    T: Num + Signed + Copy,
 {
     pub x: T,
     pub y: T,
--- src/coordinate/unsigned_coordinate.rs
+++ src/coordinate/unsigned_coordinate.rs
@@ -1,11 +1,15 @@
+use argentum_game_coordinate_system_macros::Coordinate;
 use num::{Num, Unsigned};
+
+use super::coordinate_trait::CoordinateTrait;
 
 #[cfg(test)]
 mod tests;
 
+#[derive(Coordinate)]
 pub struct UnsignedCoordinate<T>
 where
-    T: Num + Unsigned,
+    T: Num + Unsigned + Copy,
 {
     pub x: T,
     pub y: T,
