332d0f121d3227d50db00c8716f8b2ea68e5d85e
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-27 12:56:31 +0200

    Fixed unit testing suites, new coordinate types are now just as capable as the legacy type

--- src/coordinate/signed_coordinate/tests.rs
+++ src/coordinate/signed_coordinate/tests.rs
@@ -3,203 +3,203 @@ use quickcheck::quickcheck;
 use super::*;
 
 quickcheck! {
-    fn new(x: CoordinateType, y: CoordinateType, z: CoordinateType) -> bool {
-        let result = Coordinate::new(x, y, z);
-        let expected = Coordinate { x, y, z };
+    fn new(x: isize, y: isize, z: isize) -> bool {
+        let result = SignedCoordinate::new(x, y, z);
+        let expected = SignedCoordinate { x, y, z };
         result == expected
     }
 }
 
 quickcheck! {
-    fn arbitrary(coord: Coordinate) -> bool {
-        let x_fine = coord.x >= Coordinate::MIN.x && coord.x <= Coordinate::MAX.x;
-        let y_fine = coord.y >= Coordinate::MIN.y && coord.y <= Coordinate::MAX.y;
-        let z_fine = coord.z >= Coordinate::MIN.z && coord.z <= Coordinate::MAX.z;
+    fn arbitrary(coord: SignedCoordinate<isize>) -> bool {
+        let x_fine = coord.x >= SignedCoordinate::MIN.x && coord.x <= SignedCoordinate::MAX.x;
+        let y_fine = coord.y >= SignedCoordinate::MIN.y && coord.y <= SignedCoordinate::MAX.y;
+        let z_fine = coord.z >= SignedCoordinate::MIN.z && coord.z <= SignedCoordinate::MAX.z;
         x_fine && y_fine && z_fine
     }
 }
 
 #[test]
 fn min_pos() {
-    let expected = Coordinate {
-        x: CoordinateType::MIN,
-        y: CoordinateType::MIN,
-        z: CoordinateType::MIN,
+    let expected = SignedCoordinate {
+        x: isize::MIN,
+        y: isize::MIN,
+        z: isize::MIN,
     };
-    assert_eq!(expected, Coordinate::MIN);
-    assert_eq!(expected.x, CoordinateType::MIN);
-    assert_eq!(expected.y, CoordinateType::MIN);
-    assert_eq!(expected.z, CoordinateType::MIN);
+    assert_eq!(expected, SignedCoordinate::MIN);
+    assert_eq!(expected.x, isize::MIN);
+    assert_eq!(expected.y, isize::MIN);
+    assert_eq!(expected.z, isize::MIN);
 }
 
 #[test]
 fn max_pos() {
-    let expected = Coordinate {
-        x: CoordinateType::MAX,
-        y: CoordinateType::MAX,
-        z: CoordinateType::MAX,
+    let expected = SignedCoordinate {
+        x: isize::MAX,
+        y: isize::MAX,
+        z: isize::MAX,
     };
-    assert_eq!(expected, Coordinate::MAX);
-    assert_eq!(expected.x, CoordinateType::MAX);
-    assert_eq!(expected.y, CoordinateType::MAX);
-    assert_eq!(expected.z, CoordinateType::MAX);
+    assert_eq!(expected, SignedCoordinate::MAX);
+    assert_eq!(expected.x, isize::MAX);
+    assert_eq!(expected.y, isize::MAX);
+    assert_eq!(expected.z, isize::MAX);
 }
 
 #[test]
 fn display() {
-    let pos = Coordinate { x: 1, y: 2, z: 3 };
+    let pos = SignedCoordinate { x: 1, y: 2, z: 3 };
 
-    assert_eq!(pos.to_string(), "(Coordinate: 1, 2, 3)")
+    assert_eq!(pos.to_string(), "(SignedCoordinate: 1, 2, 3)")
 }
 
 quickcheck! {
-    fn splat(n: CoordinateType) -> bool {
-        let coord = Coordinate::splat(n);
+    fn splat(n: isize) -> bool {
+        let coord = SignedCoordinate::splat(n);
         coord.x == coord.y && coord.y == coord.z
     }
 }
 
 #[test]
 fn add() {
-    let result = Coordinate::new(1, 2, 3) + Coordinate::new(5, 0, 10);
-    let expected = Coordinate::new(6, 2, 13);
+    let result = SignedCoordinate::new(1, 2, 3) + SignedCoordinate::new(5, 0, 10);
+    let expected = SignedCoordinate::new(6, 2, 13);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn add_assign() {
-    let mut result = Coordinate::new(10, 15, 30);
-    result += Coordinate::new(-5, 10, 23);
-    let expected = Coordinate::new(5, 25, 53);
+    let mut result = SignedCoordinate::new(10, 15, 30);
+    result += SignedCoordinate::new(-5, 10, 23);
+    let expected = SignedCoordinate::new(5, 25, 53);
     assert_eq!(result, expected);
 }
 
 #[test]
 #[should_panic]
 fn add_overflow() {
-    let _ = Coordinate::MAX + Coordinate::splat(1);
+    let _ = SignedCoordinate::MAX + SignedCoordinate::splat(1);
 }
 
 #[test]
 fn sub() {
-    let result = Coordinate::new(1, 2, 3) - Coordinate::new(5, 0, 10);
-    let expected = Coordinate::new(-4, 2, -7);
+    let result = SignedCoordinate::new(1, 2, 3) - SignedCoordinate::new(5, 0, 10);
+    let expected = SignedCoordinate::new(-4, 2, -7);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn sub_assign() {
-    let mut result = Coordinate::new(10, 15, 30);
-    result -= Coordinate::new(-5, 10, 23);
-    let expected = Coordinate::new(15, 5, 7);
+    let mut result = SignedCoordinate::new(10, 15, 30);
+    result -= SignedCoordinate::new(-5, 10, 23);
+    let expected = SignedCoordinate::new(15, 5, 7);
     assert_eq!(result, expected);
 }
 
 #[test]
 #[should_panic]
 fn sub_overflow() {
-    let _ = Coordinate::MIN - Coordinate::splat(1);
+    let _ = SignedCoordinate::MIN - SignedCoordinate::splat(1);
 }
 
 #[test]
 fn mul() {
-    let result = Coordinate::new(5, 2, 4) * Coordinate::new(1, 0, 2);
-    let expected = Coordinate::new(5, 0, 8);
+    let result = SignedCoordinate::new(5, 2, 4) * SignedCoordinate::new(1, 0, 2);
+    let expected = SignedCoordinate::new(5, 0, 8);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn mul_assign() {
-    let mut result = Coordinate::new(3, 360, 2);
-    result *= Coordinate::new(-5, 2, 7);
-    let expected = Coordinate::new(-15, 720, 14);
+    let mut result = SignedCoordinate::new(3, 360, 2);
+    result *= SignedCoordinate::new(-5, 2, 7);
+    let expected = SignedCoordinate::new(-15, 720, 14);
     assert_eq!(result, expected);
 }
 
 #[test]
 #[should_panic]
 fn mul_overflow() {
-    let _ = Coordinate::MAX * Coordinate::splat(2);
+    let _ = SignedCoordinate::MAX * SignedCoordinate::splat(2);
 }
 
 #[test]
 fn div() {
-    let result = Coordinate::new(5, 2, 4) / Coordinate::new(1, 2, 2);
-    let expected = Coordinate::new(5, 1, 2);
+    let result = SignedCoordinate::new(5, 2, 4) / SignedCoordinate::new(1, 2, 2);
+    let expected = SignedCoordinate::new(5, 1, 2);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn div_assign() {
-    let mut result = Coordinate::new(5, 360, 24);
-    result /= Coordinate::new(-5, 2, 4);
-    let expected = Coordinate::new(-1, 180, 6);
+    let mut result = SignedCoordinate::new(5, 360, 24);
+    result /= SignedCoordinate::new(-5, 2, 4);
+    let expected = SignedCoordinate::new(-1, 180, 6);
     assert_eq!(result, expected);
 }
 
 #[test]
 #[should_panic]
 fn div_by_zero() {
-    let _ = Coordinate::MAX / Coordinate::splat(0);
+    let _ = SignedCoordinate::MAX / SignedCoordinate::splat(0);
 }
 
 #[test]
 fn neg() {
-    let result = -Coordinate::splat(7);
-    let expected = Coordinate::splat(-7);
+    let result = -SignedCoordinate::splat(7);
+    let expected = SignedCoordinate::splat(-7);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn bit_and() {
-    let coord_a = Coordinate::new(0b10101010, 0b00001111, 0b10101100);
-    let coord_b = Coordinate::splat(0b11001100);
+    let coord_a = SignedCoordinate::new(0b10101010, 0b00001111, 0b10101100);
+    let coord_b = SignedCoordinate::splat(0b11001100);
     let result = coord_a & coord_b;
-    let expected = Coordinate::new(0b10001000, 0b00001100, 0b10001100);
+    let expected = SignedCoordinate::new(0b10001000, 0b00001100, 0b10001100);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn bit_and_assign() {
-    let mut coord_a = Coordinate::new(0b10101010, 0b00001111, 0b10101100);
-    let coord_b = Coordinate::splat(0b11001100);
+    let mut coord_a = SignedCoordinate::new(0b10101010, 0b00001111, 0b10101100);
+    let coord_b = SignedCoordinate::splat(0b11001100);
     coord_a &= coord_b;
-    let expected = Coordinate::new(0b10001000, 0b00001100, 0b10001100);
+    let expected = SignedCoordinate::new(0b10001000, 0b00001100, 0b10001100);
     assert_eq!(coord_a, expected);
 }
 
 #[test]
 fn bit_or() {
-    let coord_a = Coordinate::new(0b10101010, 0b00001111, 0b10101100);
-    let coord_b = Coordinate::splat(0b11001100);
+    let coord_a = SignedCoordinate::new(0b10101010, 0b00001111, 0b10101100);
+    let coord_b = SignedCoordinate::splat(0b11001100);
     let result = coord_a | coord_b;
-    let expected = Coordinate::new(0b11101110, 0b11001111, 0b11101100);
+    let expected = SignedCoordinate::new(0b11101110, 0b11001111, 0b11101100);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn bit_or_assign() {
-    let mut coord_a = Coordinate::new(0b10101010, 0b00001111, 0b10101100);
-    let coord_b = Coordinate::splat(0b11001100);
+    let mut coord_a = SignedCoordinate::new(0b10101010, 0b00001111, 0b10101100);
+    let coord_b = SignedCoordinate::splat(0b11001100);
     coord_a |= coord_b;
-    let expected = Coordinate::new(0b11101110, 0b11001111, 0b11101100);
+    let expected = SignedCoordinate::new(0b11101110, 0b11001111, 0b11101100);
     assert_eq!(coord_a, expected);
 }
 
 #[test]
 fn bit_xor() {
-    let coord_a = Coordinate::new(0b10101010, 0b00001111, 0b10101100);
-    let coord_b = Coordinate::splat(0b11001100);
+    let coord_a = SignedCoordinate::new(0b10101010, 0b00001111, 0b10101100);
+    let coord_b = SignedCoordinate::splat(0b11001100);
     let result = coord_a ^ coord_b;
-    let expected = Coordinate::new(0b01100110, 0b11000011, 0b01100000);
+    let expected = SignedCoordinate::new(0b01100110, 0b11000011, 0b01100000);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn bit_xor_assign() {
-    let mut coord_a = Coordinate::new(0b10101010, 0b00001111, 0b10101100);
-    let coord_b = Coordinate::splat(0b11001100);
+    let mut coord_a = SignedCoordinate::new(0b10101010, 0b00001111, 0b10101100);
+    let coord_b = SignedCoordinate::splat(0b11001100);
     coord_a ^= coord_b;
-    let expected = Coordinate::new(0b01100110, 0b11000011, 0b01100000);
+    let expected = SignedCoordinate::new(0b01100110, 0b11000011, 0b01100000);
     assert_eq!(coord_a, expected);
 }
--- src/coordinate/unsigned_coordinate/tests.rs
+++ src/coordinate/unsigned_coordinate/tests.rs
@@ -3,196 +3,196 @@ use quickcheck::quickcheck;
 use super::*;
 
 quickcheck! {
-    fn new(x: CoordinateType, y: CoordinateType, z: CoordinateType) -> bool {
-        let result = Coordinate::new(x, y, z);
-        let expected = Coordinate { x, y, z };
+    fn new(x: usize, y: usize, z: usize) -> bool {
+        let result = UnsignedCoordinate::new(x, y, z);
+        let expected = UnsignedCoordinate { x, y, z };
         result == expected
     }
 }
 
 quickcheck! {
-    fn arbitrary(coord: Coordinate) -> bool {
-        let x_fine = coord.x >= Coordinate::MIN.x && coord.x <= Coordinate::MAX.x;
-        let y_fine = coord.y >= Coordinate::MIN.y && coord.y <= Coordinate::MAX.y;
-        let z_fine = coord.z >= Coordinate::MIN.z && coord.z <= Coordinate::MAX.z;
+    fn arbitrary(coord: UnsignedCoordinate<usize>) -> bool {
+        let x_fine = coord.x >= UnsignedCoordinate::MIN.x && coord.x <= UnsignedCoordinate::MAX.x;
+        let y_fine = coord.y >= UnsignedCoordinate::MIN.y && coord.y <= UnsignedCoordinate::MAX.y;
+        let z_fine = coord.z >= UnsignedCoordinate::MIN.z && coord.z <= UnsignedCoordinate::MAX.z;
         x_fine && y_fine && z_fine
     }
 }
 
 #[test]
 fn min_pos() {
-    let expected = Coordinate {
-        x: CoordinateType::MIN,
-        y: CoordinateType::MIN,
-        z: CoordinateType::MIN,
+    let expected = UnsignedCoordinate {
+        x: usize::MIN,
+        y: usize::MIN,
+        z: usize::MIN,
     };
-    assert_eq!(expected, Coordinate::MIN);
-    assert_eq!(expected.x, CoordinateType::MIN);
-    assert_eq!(expected.y, CoordinateType::MIN);
-    assert_eq!(expected.z, CoordinateType::MIN);
+    assert_eq!(expected, UnsignedCoordinate::MIN);
+    assert_eq!(expected.x, usize::MIN);
+    assert_eq!(expected.y, usize::MIN);
+    assert_eq!(expected.z, usize::MIN);
 }
 
 #[test]
 fn max_pos() {
-    let expected = Coordinate {
-        x: CoordinateType::MAX,
-        y: CoordinateType::MAX,
-        z: CoordinateType::MAX,
+    let expected = UnsignedCoordinate {
+        x: usize::MAX,
+        y: usize::MAX,
+        z: usize::MAX,
     };
-    assert_eq!(expected, Coordinate::MAX);
-    assert_eq!(expected.x, CoordinateType::MAX);
-    assert_eq!(expected.y, CoordinateType::MAX);
-    assert_eq!(expected.z, CoordinateType::MAX);
+    assert_eq!(expected, UnsignedCoordinate::MAX);
+    assert_eq!(expected.x, usize::MAX);
+    assert_eq!(expected.y, usize::MAX);
+    assert_eq!(expected.z, usize::MAX);
 }
 
 #[test]
 fn display() {
-    let pos = Coordinate { x: 1, y: 2, z: 3 };
+    let pos = UnsignedCoordinate::<usize> { x: 1, y: 2, z: 3 };
 
-    assert_eq!(pos.to_string(), "(Coordinate: 1, 2, 3)")
+    assert_eq!(pos.to_string(), "(UnsignedCoordinate: 1, 2, 3)")
 }
 
 quickcheck! {
-    fn splat(n: CoordinateType) -> bool {
-        let coord = Coordinate::splat(n);
+    fn splat(n: usize) -> bool {
+        let coord = UnsignedCoordinate::splat(n);
         coord.x == coord.y && coord.y == coord.z
     }
 }
 
 #[test]
 fn add() {
-    let result = Coordinate::new(1, 2, 3) + Coordinate::new(5, 0, 10);
-    let expected = Coordinate::new(6, 2, 13);
+    let result = UnsignedCoordinate::<usize>::new(1, 2, 3) + UnsignedCoordinate::new(5, 0, 10);
+    let expected = UnsignedCoordinate::new(6, 2, 13);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn add_assign() {
-    let mut result = Coordinate::new(10, 15, 30);
-    result += Coordinate::new(-5, 10, 23);
-    let expected = Coordinate::new(5, 25, 53);
+    let mut result = UnsignedCoordinate::<usize>::new(10, 15, 30);
+    result += UnsignedCoordinate::new(5, 10, 23);
+    let expected = UnsignedCoordinate::new(15, 25, 53);
     assert_eq!(result, expected);
 }
 
 #[test]
 #[should_panic]
 fn add_overflow() {
-    let _ = Coordinate::MAX + Coordinate::splat(1);
+    let _ = UnsignedCoordinate::<usize>::MAX + UnsignedCoordinate::splat(1);
 }
 
 #[test]
 fn sub() {
-    let result = Coordinate::new(1, 2, 3) - Coordinate::new(5, 0, 10);
-    let expected = Coordinate::new(-4, 2, -7);
+    let result = UnsignedCoordinate::<usize>::new(5, 2, 10) - UnsignedCoordinate::new(1, 0, 3);
+    let expected = UnsignedCoordinate::new(4, 2, 7);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn sub_assign() {
-    let mut result = Coordinate::new(10, 15, 30);
-    result -= Coordinate::new(-5, 10, 23);
-    let expected = Coordinate::new(15, 5, 7);
+    let mut result = UnsignedCoordinate::<usize>::new(10, 15, 30);
+    result -= UnsignedCoordinate::new(5, 10, 23);
+    let expected = UnsignedCoordinate::new(5, 5, 7);
     assert_eq!(result, expected);
 }
 
 #[test]
 #[should_panic]
 fn sub_overflow() {
-    let _ = Coordinate::MIN - Coordinate::splat(1);
+    let _ = UnsignedCoordinate::<usize>::MIN - UnsignedCoordinate::splat(1);
 }
 
 #[test]
 fn mul() {
-    let result = Coordinate::new(5, 2, 4) * Coordinate::new(1, 0, 2);
-    let expected = Coordinate::new(5, 0, 8);
+    let result = UnsignedCoordinate::<usize>::new(5, 2, 4) * UnsignedCoordinate::new(1, 0, 2);
+    let expected = UnsignedCoordinate::new(5, 0, 8);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn mul_assign() {
-    let mut result = Coordinate::new(3, 360, 2);
-    result *= Coordinate::new(-5, 2, 7);
-    let expected = Coordinate::new(-15, 720, 14);
+    let mut result = UnsignedCoordinate::<usize>::new(3, 360, 2);
+    result *= UnsignedCoordinate::new(5, 2, 7);
+    let expected = UnsignedCoordinate::new(15, 720, 14);
     assert_eq!(result, expected);
 }
 
 #[test]
 #[should_panic]
 fn mul_overflow() {
-    let _ = Coordinate::MAX * Coordinate::splat(2);
+    let _ = UnsignedCoordinate::<usize>::MAX * UnsignedCoordinate::splat(2);
 }
 
 #[test]
 fn div() {
-    let result = Coordinate::new(5, 2, 4) / Coordinate::new(1, 2, 2);
-    let expected = Coordinate::new(5, 1, 2);
+    let result = UnsignedCoordinate::<usize>::new(5, 2, 4) / UnsignedCoordinate::new(1, 2, 2);
+    let expected = UnsignedCoordinate::new(5, 1, 2);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn div_assign() {
-    let mut result = Coordinate::new(5, 360, 24);
-    result /= Coordinate::new(-5, 2, 4);
-    let expected = Coordinate::new(-1, 180, 6);
+    let mut result = UnsignedCoordinate::<usize>::new(5, 360, 24);
+    result /= UnsignedCoordinate::new(5, 2, 4);
+    let expected = UnsignedCoordinate::new(1, 180, 6);
     assert_eq!(result, expected);
 }
 
 #[test]
 #[should_panic]
 fn div_by_zero() {
-    let _ = Coordinate::MAX / Coordinate::splat(0);
+    let _ = UnsignedCoordinate::<usize>::MAX / UnsignedCoordinate::splat(0);
 }
 
 #[test]
 fn bit_and() {
-    let coord_a = Coordinate::new(0b10101010, 0b00001111, 0b10101100);
-    let coord_b = Coordinate::splat(0b11001100);
+    let coord_a = UnsignedCoordinate::<usize>::new(0b10101010, 0b00001111, 0b10101100);
+    let coord_b = UnsignedCoordinate::splat(0b11001100);
     let result = coord_a & coord_b;
-    let expected = Coordinate::new(0b10001000, 0b00001100, 0b10001100);
+    let expected = UnsignedCoordinate::new(0b10001000, 0b00001100, 0b10001100);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn bit_and_assign() {
-    let mut coord_a = Coordinate::new(0b10101010, 0b00001111, 0b10101100);
-    let coord_b = Coordinate::splat(0b11001100);
+    let mut coord_a = UnsignedCoordinate::<usize>::new(0b10101010, 0b00001111, 0b10101100);
+    let coord_b = UnsignedCoordinate::splat(0b11001100);
     coord_a &= coord_b;
-    let expected = Coordinate::new(0b10001000, 0b00001100, 0b10001100);
+    let expected = UnsignedCoordinate::new(0b10001000, 0b00001100, 0b10001100);
     assert_eq!(coord_a, expected);
 }
 
 #[test]
 fn bit_or() {
-    let coord_a = Coordinate::new(0b10101010, 0b00001111, 0b10101100);
-    let coord_b = Coordinate::splat(0b11001100);
+    let coord_a = UnsignedCoordinate::<usize>::new(0b10101010, 0b00001111, 0b10101100);
+    let coord_b = UnsignedCoordinate::splat(0b11001100);
     let result = coord_a | coord_b;
-    let expected = Coordinate::new(0b11101110, 0b11001111, 0b11101100);
+    let expected = UnsignedCoordinate::new(0b11101110, 0b11001111, 0b11101100);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn bit_or_assign() {
-    let mut coord_a = Coordinate::new(0b10101010, 0b00001111, 0b10101100);
-    let coord_b = Coordinate::splat(0b11001100);
+    let mut coord_a = UnsignedCoordinate::<usize>::new(0b10101010, 0b00001111, 0b10101100);
+    let coord_b = UnsignedCoordinate::splat(0b11001100);
     coord_a |= coord_b;
-    let expected = Coordinate::new(0b11101110, 0b11001111, 0b11101100);
+    let expected = UnsignedCoordinate::new(0b11101110, 0b11001111, 0b11101100);
     assert_eq!(coord_a, expected);
 }
 
 #[test]
 fn bit_xor() {
-    let coord_a = Coordinate::new(0b10101010, 0b00001111, 0b10101100);
-    let coord_b = Coordinate::splat(0b11001100);
+    let coord_a = UnsignedCoordinate::<usize>::new(0b10101010, 0b00001111, 0b10101100);
+    let coord_b = UnsignedCoordinate::splat(0b11001100);
     let result = coord_a ^ coord_b;
-    let expected = Coordinate::new(0b01100110, 0b11000011, 0b01100000);
+    let expected = UnsignedCoordinate::new(0b01100110, 0b11000011, 0b01100000);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn bit_xor_assign() {
-    let mut coord_a = Coordinate::new(0b10101010, 0b00001111, 0b10101100);
-    let coord_b = Coordinate::splat(0b11001100);
+    let mut coord_a = UnsignedCoordinate::<usize>::new(0b10101010, 0b00001111, 0b10101100);
+    let coord_b = UnsignedCoordinate::splat(0b11001100);
     coord_a ^= coord_b;
-    let expected = Coordinate::new(0b01100110, 0b11000011, 0b01100000);
+    let expected = UnsignedCoordinate::new(0b01100110, 0b11000011, 0b01100000);
     assert_eq!(coord_a, expected);
 }
