9c6972c3b61c94819ac8dd4c4b769e75f7db3a23
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-31 19:09:08 +0200

    Made `CoordinateTrait` a supertrait of all operations I implement using the macro, `Sized`, `Display` and `quickcheck::Arbitrary` The only trait that I don't require here is `std::ops::Neg` as it's not compatible with unsigned values.

--- src/coordinate/coordinate_trait.rs
+++ src/coordinate/coordinate_trait.rs
@@ -1,4 +1,29 @@
-pub trait CoordinateTrait {
+use std::{fmt::Display, ops::*};
+
+use quickcheck::Arbitrary;
+
+pub trait CoordinateTrait:
+    Sized
+    + Add
+    + AddAssign
+    + Sub
+    + SubAssign
+    + Mul
+    + MulAssign
+    + Div
+    + DivAssign
+    + Rem
+    + RemAssign
+    + BitAnd
+    + BitAndAssign
+    + BitOr
+    + BitOrAssign
+    + BitXor
+    + BitXorAssign
+    + Not
+    + Display
+    + Arbitrary
+{
     type Type;
     type FieldType;
 
