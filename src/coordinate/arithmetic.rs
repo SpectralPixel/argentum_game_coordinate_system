9d82003f554d762fcabf1fbcb02c904a7ba5937b
Emil Engelbert <emil.engelbert@gmail.com>
2024-10-12 19:13:31 +0200

    Revert "Implement `Mul` on `Coord` for `T` and `Self`"
    This reverts commit 9f4cb2752aec2f3d38c5829bb23c0a0af7b91e1c.

--- src/coordinate/arithmetic.rs
+++ src/coordinate/arithmetic.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, ops::{Add, BitAnd, BitOr, BitXor, Mul, Not, Sub}};
+use std::{fmt::Display, ops::{Add, BitAnd, BitOr, BitXor, Not, Sub}};
 
 use min_max_traits::{Max, Min};
 use num::{CheckedAdd, CheckedDiv, CheckedMul, CheckedSub, Integer};
@@ -114,62 +114,6 @@ where
             T::checked_sub(&self.x, &rhs).unwrap_or_else(panic),
             T::checked_sub(&self.y, &rhs).unwrap_or_else(panic),
             T::checked_sub(&self.z, &rhs).unwrap_or_else(panic),
-        )
-    }
-}
-
-impl<T> Mul<Self> for Coord<T>
-where
-    T: Integer
-        + Copy
-        + CheckedAdd
-        + CheckedSub
-        + CheckedMul
-        + CheckedDiv
-        + Display
-        + Max
-        + Min
-        + Arbitrary
-        + BitAnd<Output = T>
-        + BitOr<Output = T>
-        + BitXor<Output = T>
-        + Not<Output = T>,
-{
-    type Output = Coord<T>;
-    fn mul(self, rhs: Self) -> Self::Output {
-        let panic = || panic!("{} cannot be multiplied by {}! This may be caused by integer overflow.", self, rhs);
-        Self::new(
-            T::checked_mul(&self.x, &rhs.x).unwrap_or_else(panic),
-            T::checked_mul(&self.y, &rhs.y).unwrap_or_else(panic),
-            T::checked_mul(&self.z, &rhs.z).unwrap_or_else(panic),
-        )
-    }
-}
-
-impl<T> Mul<T> for Coord<T>
-where
-    T: Integer
-        + Copy
-        + CheckedAdd
-        + CheckedSub
-        + CheckedMul
-        + CheckedDiv
-        + Display
-        + Max
-        + Min
-        + Arbitrary
-        + BitAnd<Output = T>
-        + BitOr<Output = T>
-        + BitXor<Output = T>
-        + Not<Output = T>,
-{
-    type Output = Coord<T>;
-    fn mul(self, rhs: T) -> Self::Output {
-        let panic = || panic!("{} cannot be multiplied by {}! This may be caused by integer overflow.", self, rhs);
-        Self::new(
-            T::checked_mul(&self.x, &rhs).unwrap_or_else(panic),
-            T::checked_mul(&self.y, &rhs).unwrap_or_else(panic),
-            T::checked_mul(&self.z, &rhs).unwrap_or_else(panic),
         )
     }
 }
