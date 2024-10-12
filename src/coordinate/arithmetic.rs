c01cc72ff990e1a8439295cb36af1ffe42781c7c
Emil Engelbert <emil.engelbert@gmail.com>
2024-10-12 18:10:40 +0200

    Implement `Add` on `Coord` for `T` and `Self`

--- src/coordinate/arithmetic.rs
+++ src/coordinate/arithmetic.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, ops::{Add, BitAnd, BitOr, BitXor, Not}};
+use std::{fmt::Display, ops::{Add, BitAnd, BitOr, BitXor, Not, Sub}};
 
 use min_max_traits::{Max, Min};
 use num::{CheckedAdd, CheckedDiv, CheckedMul, CheckedSub, Integer};
@@ -58,6 +58,62 @@ where
             T::checked_add(&self.x, &rhs).unwrap_or_else(panic),
             T::checked_add(&self.y, &rhs).unwrap_or_else(panic),
             T::checked_add(&self.z, &rhs).unwrap_or_else(panic),
+        )
+    }
+}
+
+impl<T> Sub<Self> for Coord<T>
+where
+    T: Integer
+        + Copy
+        + CheckedAdd
+        + CheckedSub
+        + CheckedMul
+        + CheckedDiv
+        + Display
+        + Max
+        + Min
+        + Arbitrary
+        + BitAnd<Output = T>
+        + BitOr<Output = T>
+        + BitXor<Output = T>
+        + Not<Output = T>,
+{
+    type Output = Coord<T>;
+    fn sub(self, rhs: Self) -> Self::Output {
+        let panic = || panic!("{} cannot be subtracted by {}! This may be caused by integer overflow.", self, rhs);
+        Self::new(
+            T::checked_sub(&self.x, &rhs.x).unwrap_or_else(panic),
+            T::checked_sub(&self.y, &rhs.y).unwrap_or_else(panic),
+            T::checked_sub(&self.z, &rhs.z).unwrap_or_else(panic),
+        )
+    }
+}
+
+impl<T> Sub<T> for Coord<T>
+where
+    T: Integer
+        + Copy
+        + CheckedAdd
+        + CheckedSub
+        + CheckedMul
+        + CheckedDiv
+        + Display
+        + Max
+        + Min
+        + Arbitrary
+        + BitAnd<Output = T>
+        + BitOr<Output = T>
+        + BitXor<Output = T>
+        + Not<Output = T>,
+{
+    type Output = Coord<T>;
+    fn sub(self, rhs: T) -> Self::Output {
+        let panic = || panic!("{} cannot be subtracted by {}! This may be caused by integer overflow.", self, rhs);
+        Self::new(
+            T::checked_sub(&self.x, &rhs).unwrap_or_else(panic),
+            T::checked_sub(&self.y, &rhs).unwrap_or_else(panic),
+            T::checked_sub(&self.z, &rhs).unwrap_or_else(panic),
         )
     }
 }
