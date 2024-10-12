33f6be4c9a6b6aa76b95684e522cc900ebad96d4
Emil Engelbert <emil.engelbert@gmail.com>
2024-10-12 18:07:11 +0200

    Implement `Add` on `Coord` for `T`

--- src/coordinate/arithmetic.rs
+++ src/coordinate/arithmetic.rs
@@ -33,3 +33,31 @@ where
         )
     }
 }
+
+impl<T> Add<T> for Coord<T>
+where
+    T: Integer
+        + Copy
+        + CheckedAdd
+        + CheckedSub
+        + CheckedMul
+        + CheckedDiv
+        + Display
+        + Max
+        + Min
+        + Arbitrary
+        + BitAnd<Output = T>
+        + BitOr<Output = T>
+        + BitXor<Output = T>
+        + Not<Output = T>,
+{
+    type Output = Coord<T>;
+    fn add(self, rhs: T) -> Self::Output {
+        let panic = || panic!("{} cannot be added by {}! This may be caused by integer overflow.", self, rhs);
+        Self::new(
+            T::checked_add(&self.x, &rhs).unwrap_or_else(panic),
+            T::checked_add(&self.y, &rhs).unwrap_or_else(panic),
+            T::checked_add(&self.z, &rhs).unwrap_or_else(panic),
+        )
+    }
+}
