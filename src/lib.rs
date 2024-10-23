0dc9ec755f87f82b5309cf262b984fcf614913cb
Emil Engelbert <emil.engelbert@gmail.com>
2024-10-23 20:57:56 +0200

    Expand on the outer doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 //! # Argentum - Coordinate System
 //!
-//! `argentum_game_coordinate_system` contains the coordinate system that is used by
-//! Argentum and some utilities related to it.
+//! `argentum_game_coordinate_system` contains the coordinate system that is
+//! used by Argentum and some utilities related to it.
+//! 
+//! Import the `prelude` to gain access to all the crate's types.
+//! 
+//! The correct type to use when interfacing with Argentum is `Coordinate`.
+//! 
+//! Feel free to make other similar types by creating type aliases for
+//! `Coord<T>`.
 //!
 //! For more information about Argentum, see the `argentum_game` crate.
 
